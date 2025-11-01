9244b21ef802e246958f77f91f8193a3666919e5
Pavel Leushin <leushinps@outlook.com>
2025-11-01 11:40:41 +0500

    Task7

--- Tyuiu.LeushinP.Sprint2.Task7.V10.Test/DataServiceTest.cs
+++ Tyuiu.LeushinP.Sprint2.Task7.V10.Test/DataServiceTest.cs
@@ -1,4 +1,4 @@
-
+using NUnit.Framework;
 using Tyuiu.LeushinP.Sprint2.Task7.V10.Lib;
 
 namespace Tyuiu.LeushinP.Sprint2.Task7.V10.Test
@@ -16,5 +16,25 @@ namespace Tyuiu.LeushinP.Sprint2.Task7.V10.Test
             Assert.IsTrue(ds.CheckDotInShadedArea(0.5, -1.5));
         }
 
+        [Test]
+        public void ValidCheckDotInShadedArea_Outside()
+        {
+            DataService ds = new DataService();
+
+
+            Assert.IsFalse(ds.CheckDotInShadedArea(0, 1));   
+            Assert.IsFalse(ds.CheckDotInShadedArea(2, 1));   
+            Assert.IsFalse(ds.CheckDotInShadedArea(1, 1));   
+        }
+
+        [Test]
+        public void ValidCheckDotInShadedArea_Boundary()
+        {
+            DataService ds = new DataService();
+
+
+            Assert.IsTrue(ds.CheckDotInShadedArea(1, -1));   
+            Assert.IsTrue(ds.CheckDotInShadedArea(0, 0));    
+        }
     }
 }

7fed8fe217bebfb3a7ec214243c1d4b365b9a040
Pavel Leushin <leushinps@outlook.com>
2025-11-01 11:35:30 +0500

    Task7

--- Tyuiu.LeushinP.Sprint2.Task7.V10.Lib/DataService.cs
+++ Tyuiu.LeushinP.Sprint2.Task7.V10.Lib/DataService.cs
@@ -13,5 +13,12 @@ namespace Tyuiu.LeushinP.Sprint2.Task7.V10.Lib
 
             return Parabola && Line;
         }
+        public double CalculateFunction(double x)
+        {
+
+            double result = Math.Pow(x, 3) + 2 * Math.Pow(x, 2) - 3 * x + 5;
+
+            return Math.Round(result, 3);
+        }
     }
 }

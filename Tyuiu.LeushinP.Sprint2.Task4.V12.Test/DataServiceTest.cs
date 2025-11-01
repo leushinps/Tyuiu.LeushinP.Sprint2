bd8ab6659daebf2f401f50266795eb174db3ee4d
Pavel Leushin <leushinps@outlook.com>
2025-11-01 06:07:44 +0500

    Task4

--- Tyuiu.LeushinP.Sprint2.Task4.V12.Test/DataServiceTest.cs
+++ Tyuiu.LeushinP.Sprint2.Task4.V12.Test/DataServiceTest.cs
@@ -1,20 +1,27 @@
-using NUnit.Framework;
-using Tyuiu.LeushinP.Sprint2.Task4.V12.Lib;
+using System;
+using tyuiu.cources.programming.interfaces.Sprint2;
 
-namespace Tyuiu.LeushinP.Sprint2.Task4.V12.Test
+namespace Tyuiu.LeushinP.Sprint2.Task4.V12.Lib
 {
-    [TestFixture]
-    public class DataServiceTest
+    public class DataService : ISprint2Task4V12
     {
-        [Test]
-        public void ValidCalculateFirstCondition()
+        public double Calculate(double x, double y)
         {
-            DataService ds = new DataService();
-            double x = 16;
-            double y = 1;
-            double wait = 100; 
-            double res = ds.Calculate(x, y);
-            Assert.AreEqual(wait, res);
+            if (y == 0)
+            {
+                throw new ArgumentException("Значение y не может быть равно 0");
+            }
+
+            if (x < 0)
+            {
+                throw new ArgumentException("Значение x не может быть отрицательным для извлечения корня");
+            }
+
+            double result = Math.Sqrt(x) > 2 * y
+                ? Math.Pow(7 + 3.0 / y, x)
+                : (Math.Pow(x, y) - Math.Sin(y) + 10) / (Math.Pow(y, x) - Math.Sin(x) + 12);
+
+            return Math.Round(result, 3);
         }
     }
 }

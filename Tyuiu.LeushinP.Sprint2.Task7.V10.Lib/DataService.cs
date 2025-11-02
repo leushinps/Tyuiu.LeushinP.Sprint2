48435a462baa2d52daedcb6d3e832a57f7422576
Pavel Leushin <leushinps@outlook.com>
2025-11-03 00:06:09 +0500

    Task7

--- Tyuiu.LeushinP.Sprint2.Task7.V10.Lib/DataService.cs
+++ Tyuiu.LeushinP.Sprint2.Task7.V10.Lib/DataService.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+
 using tyuiu.cources.programming.interfaces.Sprint2;
+
 
 namespace Tyuiu.LeushinP.Sprint2.Task7.V10.Lib
 {
@@ -7,23 +9,11 @@ namespace Tyuiu.LeushinP.Sprint2.Task7.V10.Lib
     {
         public bool CheckDotInShadedArea(double x, double y)
         {
-
             bool aboveParabola = y >= x * x - 2;
-
-
             bool belowLineYEqualsX = y <= x;
-
-
             bool aboveLineYEqualsMinusX = y >= -x;
 
-            if (aboveParabola && belowLineYEqualsX && aboveLineYEqualsMinusX)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
+            return aboveParabola && belowLineYEqualsX && aboveLineYEqualsMinusX;
         }
     }
 }

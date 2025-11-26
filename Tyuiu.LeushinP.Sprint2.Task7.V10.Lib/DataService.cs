dc4eb14603183dd076aab57f3db5770344270f31
Pavel Leushin <leushinps@outlook.com>
2025-11-27 00:03:34 +0500

    Task7

--- Tyuiu.LeushinP.Sprint2.Task7.V10.Lib/DataService.cs
+++ Tyuiu.LeushinP.Sprint2.Task7.V10.Lib/DataService.cs
@@ -6,20 +6,11 @@ namespace Tyuiu.LeushinP.Sprint2.Task7.V10.Lib
     public class DataService : ISprint2Task7V10
     {
         public bool CheckDotInShadedArea(double x, double y)
-        {
-            bool res;
-            if ((y >= Math.Pow(x, 2) - 2) && (x <= 2) && (x >= -2) && (y >= -2) && (y <= 2))
             {
-                res = true;
+                if ((y >= Math.Pow(x, 2) - 2 && y <= x) || (y >= Math.Pow(x, 2) - 2 && y <= -x))
+                    return true;
+                else
+                    return false;
             }
-            else
-            {
-                res = false;
-            }
-            return res;
-
-
-
         }
     }
-}
--- Tyuiu.LeushinP.Sprint2.Task7.V10/Program.cs
+++ Tyuiu.LeushinP.Sprint2.Task7.V10/Program.cs
@@ -31,17 +31,13 @@ namespace Tyuiu.LeushinP.Sprint2.Task7.V10
             Console.Write("Введите координату Y: ");
             double y = Convert.ToDouble(Console.ReadLine());
 
-            bool res = ds.CheckDotInShadedArea(x, y);
 
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
             Console.WriteLine("***************************************************************************");
-
-            if (res)
-                Console.WriteLine("Точка находится в заштрихованной области.");
-            else
-                Console.WriteLine("Точка не находится в заштрихованной области.");
-
+            var res = ds.CheckDotInShadedArea(x, y);
+            if (res) Console.WriteLine("Точка находится в закрашенной области");
+            else Console.WriteLine("Точка находится в незакрашенной области");
             Console.ReadKey();
         }
     }
